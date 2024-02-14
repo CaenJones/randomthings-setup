7fccf940169eb9e0f90959761f1fdd79dc046551
Caen Jones <131218155+CaenJones@users.noreply.github.com>
2024-02-14 13:43:19 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt autoremove -y
-curl https://github.com/CaenJones/randomthings-setup/blob/main/README.md
+curl -O https://github.com/CaenJones/randomthings-setup/blob/main/README.md
 mv README.md /home/desktop/README.md
 sudo apt installl git python3 vim nano make wget Geany
 cat /home/desktop/readme.md
